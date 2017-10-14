e246e9bf4a665c5e4bd730bad6973c395cf56a8a
Quinton Aiken <quintonlaiken@gmail.com>
2017-10-14 15:03:15 -0700

    create media queries

---
+++ src/media_queries.js
@@ -0,0 +1,11 @@
+const breakPoints = {
+  mobile: '480px',
+  tablet: '768px'
+}
+
+const mediaQueries = {
+  mobile: `@media only screen and (max-width: ${breakPoints.mobile})`,
+  tablet: `@media only screen and (max-width: ${breakPoints.tablet})`,
+}
+
+export default mediaQueries
